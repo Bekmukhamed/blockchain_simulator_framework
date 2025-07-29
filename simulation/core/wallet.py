674c3d0a0ded43d019bccc155176077eceba63bf
Bekmukhamed Tursunbayev <60927600+Bekmukhamed@users.noreply.github.com>
2025-07-28 21:17:46 -0500

    Update wallet.py

--- simulation/core/wallet.py
+++ simulation/core/wallet.py
@@ -1,21 +1,8 @@
 import simulation.globals as sim_globals
 
 
-class Node:
-    def __init__(self, env, i):
-        self.env = env
-        self.id = i
-        self.blocks = set()
-        self.neighbors = []
-    
-    def receive(self, b):
-        yield self.env.timeout(0)
-        
-        if b.id in self.blocks:
-            return
-        self.blocks.add(b.id)
-        
-        for n in self.neighbors:
-            sim_globals.io_requests += 1
-            sim_globals.network_data += b.size
-            self.env.process(n.receive(b))
+# Wallet sends transactions into pool
+def wallet(env, wid, count, interval):
+    for _ in range(count):
+        yield env.timeout(interval)
+        sim_globals.pool.append((wid, env.now))
