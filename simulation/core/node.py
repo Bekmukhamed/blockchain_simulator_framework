f860ff8f74edfa9b180af647b6bb130a167022f4
Bekmukhamed Tursunbayev <60927600+Bekmukhamed@users.noreply.github.com>
2025-07-28 21:18:05 -0500

    Update node.py

--- simulation/core/node.py
+++ simulation/core/node.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-from simulation.core.utils import pool, io_requests, network_data
-
-class Node:
-    def __init__(self, env, i):
-        self.env = env; self.id = i; self.blocks = set(); self.neighbors = []
-    def receive(self, b):
-        yield self.env.timeout(0)
-        global network_data, io_requests
-        if b.id in self.blocks:
-            return
-        self.blocks.add(b.id)
-        for n in self.neighbors:
-            io_requests += 1
-            network_data += b.size
-            self.env.process(n.receive(b))
-=======
 import simulation.globals as sim_globals
 
 
@@ -36,4 +19,3 @@ class Node:
             sim_globals.io_requests += 1
             sim_globals.network_data += b.size
             self.env.process(n.receive(b))
->>>>>>> 54adcc3f4b790c924bff49b0e700965b2eef3270
